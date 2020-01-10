fixed food-finder, softmax prediction and implemented simple annealer

main.py
@@ -14,28 +14,59 @@ from snake import *
 
 pygame.init()
 screen = pygame.display.set_mode((420, 420))
-done = False
-m = Map(100, 100, 4)
 
-# you can change the input model here,
-# or define new standard in Snake._createNewBrain()
+# you can change the input model here, but keep the input_shape as (7,)
+# and final layer output as 4 or whatever you have as dimensions.
 # extra params for Snake in compilation:
 #  - loss_fun
 #  - opt
 #  - metrics
 model = [
-    Dense(12, input_dim = 1, activation = 'relu'),
-    Dense(8, input_dim = 12, activation = 'relu'),
-    Dense(1, activation = 'sigmoid')
+    Dense(14, input_shape = (7,), activation = 'relu'),
+    Dense(9, input_dim = 7, activation = 'relu'),
+    Dense(4, activation = 'softmax')
 ]
 
-testSnakes = [Snake(brain = model) for _ in range(1)]
-m.nextRound(testSnakes, 50)
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+i = 0
+reallydone = False
+testSnakes = [Snake(brain = model) for _ in range(5)]
+while not reallydone:
+    print("iteration", i, end = '\n')
 
-    pygame.display.flip()
-    deadSnakes = m.update()
-    m.drawGrid(screen, 10, 10)
+    m = Map(100, 100, 4)
+    m.nextRound(testSnakes, 10)
+    testSnakes = None
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done, reallydone = True, True
+
+        pygame.display.flip()
+        # if all dead, returns the list of dead snakes
+        # and terminates the game list
+        testSnakes = m.update()
+        if testSnakes != None:
+            done = True
+        m.drawGrid(screen, 10, 10)
+
+    # TODO:
+    # play with dead snakes
+    sizetable, maxsize, minsize = [], 0, np.inf
+    n = 0
+    for snake in testSnakes:
+        sizetable.append([snake.size, snake])
+        if snake.size > maxsize:
+            maxsize = snake.size
+        if snake.size < minsize:
+            minsize = snake.size
+
+        n += 1
+        print(n, "-th snake had size: ", snake.size)
+
+    testSnakes = [Snake(brain = model) if snake.size == minsize else snake for snake in testSnakes ]
+
+    i += 1
+    if i > 100:
+        reallydone = True

map.py
@@ -3,7 +3,6 @@ import random
 import numpy as np
 from snake import *
 
-
 # tile info:
 # 0: empty tile
 # 1: snake
@@ -39,59 +38,46 @@ class Map():
     def _initialize_grid(self):
         self.grid = [[0] * self.x for i in range(self.y)]
 
-    # TODO: function for creating model inputs from the map
-    # relative to the snake.
     def sitrep(self, snake):
         # access head
-        head = snake.parts[-1]
-        # print([self.safeAt(head[0]-1, head[1]),
-        #        self.safeAt(head[0]+1, head[1]),
-        #        self.safeAt(head[0], head[1]-1),
-        #        self.safeAt(head[0], head[1]+1),
-        #        *self.closestFood(*head),
-        #        snake.size])
-        return [self.safeAt(head[0]-1, head[1]),
+        head = snake.parts[0]
+        return [[self.safeAt(head[0]-1, head[1]),
                 self.safeAt(head[0]+1, head[1]),
                 self.safeAt(head[0], head[1]-1),
                 self.safeAt(head[0], head[1]+1),
                 *self.closestFood(*head),
-                snake.size]
+                snake.size]]
 
-    # spiraling out algorithm for finding closest food tile
+    # spiraling out algorithm (sorta) for finding closest food tile to x, y
     def closestFood(self, x, y):
-        for i in range(0, np.max([self.x, self.y])):
+        for i in range(1, np.max((self.x, self.y))):
             # interweave horizontal sides
-            a = np.arange(y, y - i, -1)
-            b = np.arange(y + 1, y + i)
-            spiral = np.empty((a.size + b.size,), dtype = int)
-            spiral[0::2] = a
-            spiral[1::2] = b
+            spiral = np.empty((i * 2 - 1, ), dtype = int)
+            spiral[0::2] = np.arange(y, y - i, -1)
+            spiral[1::2] = np.arange(y + 1, y + i)
 
-            # look at horizontal sides
-            if self.isInGrid(x - i, y - i) and self.isInGrid(x - i, y + i):
+            # look at horizontal sides (and check if both points are still in grid)
+            if self.isInGrid(x - i, y - i) or self.isInGrid(x - i, y + i):
                 for nx, ny in zip([x - i] * len(spiral), spiral):
                     if self.safeAt(nx, ny) == 2:
                         return x - nx, y - ny
 
-            if self.isInGrid(x + i, y - i) and self.isInGrid(x + i, y + i):
+            if self.isInGrid(x + i, y - i) or self.isInGrid(x + i, y + i):
                 for nx, ny in zip([x + i] * len(spiral), spiral):
                     if self.safeAt(nx, ny) == 2:
                         return x - nx, y - ny
 
             # interweave vertical sides
-            a = np.arange(x, x - i, -1)
-            b = np.arange(x + 1, x + i)
-            spiral = np.empty((a.size + b.size,), dtype = int)
-            spiral[0::2] = a
-            spiral[1::2] = b
+            spiral[0::2] = np.arange(x, x - i, -1)
+            spiral[1::2] = np.arange(x + 1, x + i)
 
             # look at vertical sides
-            if self.isInGrid(x - i, y - i) and self.isInGrid(x + i, y - i):
+            if self.isInGrid(x - i, y - i) or self.isInGrid(x + i, y - i):
                 for nx, ny in zip(spiral, [y - i] * len(spiral)):
                     if self.safeAt(nx, ny) == 2:
                         return x - nx, y - ny
 
-            if self.isInGrid(x - i, y + i) and self.isInGrid(x + i, y + i):
+            if self.isInGrid(x - i, y + i) or self.isInGrid(x + i, y + i):
                 for nx, ny in zip(spiral, [y + i] * len(spiral)):
                     if self.safeAt(nx, ny) == 2:
                         return x - nx, y - ny

snake.py
@@ -16,11 +16,9 @@ class Snake():
                  loss_fun = 'mean_squared_error',
                  opt = 'adam',
                  metrics = ['accuracy']):
-        if brain is None:
-            self.model = self._createNewBrain()
-        else:
-            self.model = Sequential(layers = brain)
-            self.model.compile(loss = loss_fun, optimizer = opt, metrics = metrics)
+
+        self.model = Sequential(layers = brain)
+        self.model.compile(loss = loss_fun, optimizer = opt, metrics = metrics)
 
         self.parts = deque()
         self.lifeSpan = 0
@@ -28,7 +26,7 @@ class Snake():
         self.lastDir = 0
         self.size = 0
 
-    def init(self, x, y, initialLen=3, lifeSpan=300):
+    def init(self, x, y, initialLen=3, lifeSpan=100):
         # Using a deque to store snake's parts
         # First element is the head.
         self.parts = deque()
@@ -40,19 +38,12 @@ class Snake():
         self.lastDir = 0
         self.size = initialLen
 
-    def _createNewBrain(self):
-        model = Sequential()
-        model.add(Dense(12, input_dim = 1, activation = 'relu'))
-        model.add(Dense(8, input_dim = 12, activation = 'relu'))
-        model.add(Dense(1, activation = 'sigmoid'))
-        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
-        return model
-
     # Returns if the snake is still alive.
     def step(self, sitrep, grid):
         if not self.alive:
             return False
         elif self.lifeSpan <= 0:
+            self.alive = False
             return False
 
         #### TODO: input tuning
@@ -61,10 +52,7 @@ class Snake():
         #           (nearest food loc_x - x, loc_y - y) x, y, (snake length) len]
         # ... total 7 elements
         pred = self.model.predict(sitrep)
-        self._move(np.argmax((softmaxer( pred[[ 0, 4, 5, 6 ]] ),
-                              softmaxer( pred[[ 1, 4, 5, 6 ]] ),
-                              softmaxer( pred[[ 2, 4, 5, 6 ]] ),
-                              softmaxer( pred[[ 3, 4, 5, 6 ]] ))), grid)
+        self._move(np.argmax(pred), grid)
 
         return True
 
@@ -74,6 +62,7 @@ class Snake():
     # 2: down
     # 3: left
     def _move(self, dir, grid):
+        self.lifeSpan -= 1
         x = self.parts[0][0]
         y = self.parts[0][1]
         self.lastDir = dir